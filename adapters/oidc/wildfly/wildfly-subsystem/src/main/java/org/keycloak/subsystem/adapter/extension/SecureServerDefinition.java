02db40ff3232431088dba382925e5e8df51d6341
Pedro Igor <pigor.craveiro@gmail.com>
2017-01-13 11:14:44 -0200

    Minor change

--- adapters/oidc/wildfly/wildfly-subsystem/src/main/java/org/keycloak/subsystem/adapter/extension/SecureServerDefinition.java
+++ adapters/oidc/wildfly/wildfly-subsystem/src/main/java/org/keycloak/subsystem/adapter/extension/SecureServerDefinition.java
@@ -99,7 +99,8 @@ final class SecureServerDefinition extends AbstractAdapterConfigurationDefinitio
             KeycloakHttpAuthenticationFactoryService service = new KeycloakHttpAuthenticationFactoryService(factoryName);
             ServiceTarget serviceTarget = context.getServiceTarget();
             InjectedValue<ExtensibleHttpManagement> injectedValue = new InjectedValue<>();
-            serviceTarget.addService(serviceName.append("http-management-context"), createHttpManagementConfigContextService(factoryName, injectedValue)).addDependency(context.getCapabilityServiceName(HTTP_MANAGEMENT_HTTP_EXTENSIBLE_CAPABILITY, ExtensibleHttpManagement.class), ExtensibleHttpManagement.class, injectedValue).setInitialMode(Mode.ACTIVE).install();
+            serviceTarget.addService(serviceName.append("http-management-context"), createHttpManagementConfigContextService(factoryName, injectedValue))
+                    .addDependency(context.getCapabilityServiceName(HTTP_MANAGEMENT_HTTP_EXTENSIBLE_CAPABILITY, ExtensibleHttpManagement.class), ExtensibleHttpManagement.class, injectedValue).setInitialMode(Mode.ACTIVE).install();
             serviceTarget.addService(serviceName, service).setInitialMode(Mode.ACTIVE).install();
         }
     }
